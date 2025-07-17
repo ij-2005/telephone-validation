4181c1ab34f07c29e9af62c8c26634cd66caf834
ij-2005 <officialij2005@gmail.com>
2025-07-17 15:10:31 +0800

    added logics

--- script.js
+++ script.js
@@ -9,24 +9,54 @@ const input = document.getElementById('user-input');
 const resultsContainer = document.getElementById('results-div');
 
 
+//functions
+
+function logicDoer(value){
+
+     if(value === ""){
+        alert("Please provide a phone number");
+        return;
+    }else{
+        const validated = numberValidate(value);
+        htmlAdd(validated, value);
+    }
+
+    input.value = "";
+
+}
+
+const numberValidate = (input) => {
+
+    const regex = /^(1\s?)?((\(\d{3}\))|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
+
+    if(regex.test(input)){
+        return "Valid";
+    }else{
+        return "Invalid";
+    }
+    
+}
+
+const htmlAdd = (string, input) => {
+    resultsContainer.innerHTML += `<p id="results-text">${string} US number: ${input}</p>`
+}
+
 
 
 // button listeners
 
 check.addEventListener('click', ()=>{
-    
-    if(input.value === ""){
-        alert("Please provide a phone number");
-        return;
-    }else{
-        console.log(input.value);
-        input.value = "";
-    }
-
+    logicDoer(input.value);
 });
 
 clear.addEventListener('click',()=>{
-    
     resultsContainer.innerHTML = "";
+});
+
+document.addEventListener('keydown', function(button){
+
+    if(button.code === "Enter"){
+        logicDoer(input.value);
+    }
 
 });
